ee68685808ea3dfd701053eebe795a9cb161ddbe
handsomezhou <quanjunzhou@gmail.com>
2016-11-12 01:02:52 +0800

    update note

--- QwertySearch/qwertysearch/src/main/java/com/qwertysearch/util/CommonUtil.java
+++ QwertySearch/qwertysearch/src/main/java/com/qwertysearch/util/CommonUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  [handsomezhou] 2016 The T9 search Open Source Project
+ * Copyright  [handsomezhou] 2016 The Qwerty search Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
